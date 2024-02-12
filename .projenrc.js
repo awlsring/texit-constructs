9fd0957c77457fa81971933bb64ff50f86d45d9d
Matthew Rawlings <contact@matthewrawlings.com>
2024-02-11 23:21:38 -0800

    chore(lint): fix projen file

--- .projenrc.js
+++ .projenrc.js
@@ -1,25 +1,25 @@
-const { awscdk } = require("projen");
-const { GithubCredentials } = require("projen/lib/github");
-const { NpmAccess } = require("projen/lib/javascript");
+const { awscdk } = require('projen');
+const { GithubCredentials } = require('projen/lib/github');
+const { NpmAccess } = require('projen/lib/javascript');
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: "Matthew Rawlings",
-  authorAddress: "contact@matthewrawlings.com",
-  cdkVersion: "2.127.0",
-  defaultReleaseBranch: "main",
-  name: "texit-constructs",
-  testdir: "",
-  gitignore: [".vscode"],
+  author: 'Matthew Rawlings',
+  authorAddress: 'contact@matthewrawlings.com',
+  cdkVersion: '2.127.0',
+  defaultReleaseBranch: 'main',
+  name: 'texit-constructs',
+  testdir: '',
+  gitignore: ['.vscode'],
   npmAccess: NpmAccess.PUBLIC,
   releaseToNpm: true,
   autoApproveUpgrades: true,
   autoApproveOptions: {
-    allowedUsernames: ["awlsring"],
-    secret: "GITHUB_TOKEN",
+    allowedUsernames: ['awlsring'],
+    secret: 'GITHUB_TOKEN',
   },
-  repositoryUrl: "https://github.com/mattcanemail/texit-constructs.git",
+  repositoryUrl: 'https://github.com/mattcanemail/texit-constructs.git',
   githubOptions: {
     projenCredentials: GithubCredentials.fromPersonalAccessToken(
-      "PROJEN_GITHUB_TOKEN"
+      'PROJEN_GITHUB_TOKEN',
     ),
   },
 });
